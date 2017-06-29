0423a3fb6d580deb92e57e36b63584a14c8f3fb1
Liseth Cardozo <cardozoliseth@gmail.com>
2017-06-28 20:36:52 -0700

    Create root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  #root route
+  root 'demo#index'
+  #simple route
   get 'demo/index'
+
+  #default route may go away in the future
+  #in new version of rails
+  #  get ':controller(/:action(/:id))'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
