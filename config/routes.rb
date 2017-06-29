961e900a4bb198988d03d25509c4c869462eea76
Liseth Cardozo <cardozoliseth@gmail.com>
2017-06-29 10:50:28 -0700

    Add user and links controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'links/index'
+
+  get 'user/index'
+
+  get 'user/show'
+
+  get 'user/create'
+
+  get 'user/new'
+
   #root route
   root 'demo#index'
   #simple route
@@ -6,7 +16,7 @@ Rails.application.routes.draw do
 
   #default route may go away in the future
   #in new version of rails
-  #  get ':controller(/:action(/:id))'
+  # get ':controller(/:action(/:id))'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
